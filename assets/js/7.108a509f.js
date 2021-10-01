7f4cd95bda8a023053ef9931d2339b0d121c94b7
aleversn <aleversn@users.noreply.github.com>
2021-10-01 06:19:12 +0000

    deploy: 3883bf96f3ed341b6aae40b8e53b5ea15e612eb0

--- assets/js/7.108a509f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{677:function(t,e,n){"use strict";n.r(e);var s=n(1),o=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("power-editor",{staticStyle:{width:"100%"}})],1)}),[],!1,null,null,null);e.default=o.exports}}]);
