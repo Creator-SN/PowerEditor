8d1b66abacc9a1b01cea9e390022409b93c819c1
Alever <759636538@qq.com>
2022-06-27 17:09:59 +0800

    [update]

--- src/modules/editor/src/components/custom/pasteRules/nodePasteRules.js
+++ src/modules/editor/src/components/custom/pasteRules/nodePasteRules.js
@@ -1,32 +1,32 @@
 
-import { PasteRule,callOrReturn  } from '@tiptap/core'
+import { PasteRule, callOrReturn } from '@tiptap/core'
 
 /**
  * Build an paste rule that adds a node when the
  * matched text is pasted into it.
  */
 export function nodePasteRule({
-  find,
-  type,
-  getAttributes
-}) {
-  return new PasteRule({
     find,
-    handler({ match, chain, range }) {
-      const attributes = callOrReturn(getAttributes, undefined, match)
+    type,
+    getAttributes
+}) {
+    return new PasteRule({
+        find,
+        handler({ match, chain, range }) {
+            const attributes = callOrReturn(getAttributes, undefined, match)
 
-      if (attributes === false || attributes === null) {
-        return null
-      }
+            if (attributes === false || attributes === null) {
+                return null
+            }
 
-      if (match.input) {
-        chain()
-          .deleteRange(range)
-          .insertContent({
-            type: type.name,
-            attrs: attributes,
-          })
-      }
-    },
-  })
+            if (match.input) {
+                chain()
+                    .deleteRange(range)
+                    .insertContent({
+                        type: type.name,
+                        attrs: attributes,
+                    })
+            }
+        },
+    })
 }
